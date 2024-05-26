c7661cd123d37e47e395e1e1f4ab038877a33d36
clubfullstack <165699267+clubfullstack@users.noreply.github.com>
2024-05-25 23:32:32 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gunicorn app:app --bind 0.0.0.0:$PORT
+
+# Adiciona o diretório binário do Python 3 ao PATH
+export PATH=$PATH:/usr/local/python3/bin
+
+# Instala o Gunicorn usando pip
+pip install gunicorn
+
+# Inicia a aplicação Bottle usando o Gunicorn como servidor web
+gunicorn app:app
